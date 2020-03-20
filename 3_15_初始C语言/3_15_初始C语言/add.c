ae9e224d8485371545823575448a86d69b548884
Ymenging_MT <yanmt311@163.com>
2020-03-20 23:24:19 +0800

    c

--- 3_15_初始C语言/3_15_初始C语言/add.c
+++ 3_15_初始C语言/3_15_初始C语言/add.c
@@ -3,14 +3,19 @@
 //定义一个全局变量
 //int g_val = 1;
 
+
+//static修饰全局变量
 //static int g_val = 1;
 
+
+//函数
 //int Add(int x,int y)
 //{
 //	int z = x + y;
 //	return z;
 //}
 
+//static修饰函数
 static int Add(int x, int y)
 {
 	int z = x + y;
